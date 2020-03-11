67cd16d0fe685fdf2770fe89cde193727e5a0153
unknown <tristan57360@gmail.com>
2020-03-11 09:57:12 +0100

    fix lint

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/service/auth.service';
+import { resolve } from 'url';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +10,16 @@ import { AuthService } from 'src/app/service/auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  isError;
   profileForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required],
   });
   ngOnInit() {
+    this.isError = false;
   }
   onSubmit() {
-    this.authService.SignIn(this.profileForm.value.email, this.profileForm.value.password);
+    this.authService.SignIn(this.profileForm.value.email, this.profileForm.value.password).then(res => {}, err => {this.isError = true; } );
   }
   constructor(private fb: FormBuilder, private router: Router, private authService: AuthService) { }
 }
