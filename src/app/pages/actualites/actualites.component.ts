b1125df1a2d53e0b73fb46f2e972b90f2d1ebaf3
unknown <tristan57360@gmail.com>
2020-03-11 09:31:48 +0100

    fix

--- src/app/pages/actualites/actualites.component.ts
+++ src/app/pages/actualites/actualites.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { ActualiteService } from 'src/app/service/actualite.service';
+import { Component, OnInit } from "@angular/core";
+import { ActualiteService } from "src/app/service/actualite.service";
 
-import { AuthService } from 'src/app/service/auth.service';
+import { AuthService } from "src/app/service/auth.service";
 
 @Component({
-  selector: 'app-actualites',
-  templateUrl: './actualites.component.html',
-  styleUrls: ['./actualites.component.scss']
+  selector: "app-actualites",
+  templateUrl: "./actualites.component.html",
+  styleUrls: ["./actualites.component.scss"]
 })
 export class ActualitesComponent implements OnInit {
   actualitesList;
   isAdmin;
   getActualites = () => {
-    this.actualiteService.getActualites().subscribe(res => (this.actualitesList = res.sort((a,b)=>
-    b.payload.doc.data().date.seconds - a.payload.doc.data().date.seconds)));
+    this.actualiteService
+      .getActualites()
+      .subscribe(
+        res =>
+          (this.actualitesList = res.sort(
+            (a, b) =>
+              b.payload.doc.data().date.seconds -
+              a.payload.doc.data().date.seconds
+          ))
+      );
   }
 
-  constructor(private actualiteService: ActualiteService, private authService: AuthService) {
+  constructor(
+    private actualiteService: ActualiteService,
+    private authService: AuthService
+  ) {
     this.isAdmin = this.authService.isAdmin;
   }
 
   ngOnInit() {
     this.getActualites();
   }
-
 }
